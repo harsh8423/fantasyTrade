3dca3e7f76a4e58a722fd0026b63787e66f66dfd
Harsh Tiwari <145486818+harsh8423@users.noreply.github.com>
2023-12-28 01:36:07 +0530

    first commit

--- client/src/screens/Home.js
+++ client/src/screens/Home.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useEffect } from "react";
 
 export default function Home() {
-  return (
-    <div>
-      home
-    </div>
-  )
+  const getAuth = () => {
+    const apiUrl = "https://api.upstox.com/v2/user/profile";
+    const apiMethod = "GET";
+    const apiVersion = "2.0";
+    const accessToken = "YOUR_ACTUAL_ACCESS_TOKEN";
+
+    const headers = {
+      accept: "application/json",
+      "Api-Version": apiVersion,
+      Authorization: `Bearer ${accessToken}`,
+      // Add other headers if needed, such as 'Content-Type' for request payload
+    };
+
+    fetch(apiUrl, {
+      method: apiMethod,
+      headers: headers,
+      // Add a request payload if needed (for example, body: JSON.stringify({ key: 'value' }))
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("API response:", data);
+        // Handle the API response data here
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        // Handle errors here
+      });
+
+    useEffect(() => {
+      getAuth();
+    }, []);
+  };
+  return <div>home</div>;
 }
